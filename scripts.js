bc2314dd32d4b9876c5dae58aefb232e251b33ad
Francis Malina <fmalina@pm.me>
2021-04-16 19:40:36 +0200

    Lots of text fixes

--- scripts.js
+++ scripts.js
@@ -1,3 +1,18 @@
+function slugify(string) {
+  const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìľḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;'
+  const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------'
+  const p = new RegExp(a.split('').join('|'), 'g')
+
+  return string.toString().toLowerCase()
+    .replace(/\s+/g, '') // Replace spaces with ''
+    .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+    .replace(/&/g, '') // Replace & with ''
+    .replace(/[^\w\-]+/g, '') // Remove all non-word characters
+    .replace(/\-\-+/g, '') // Replace multiple - with single -
+    .replace(/^-+/, '') // Trim - from start of text
+    .replace(/-+$/, '') // Trim - from end of text
+}
+
 function createTOC() {
 	var y = document.createElement('div');
 	y.id = 'toc';
@@ -15,11 +30,13 @@ function createTOC() {
 		var tmp = document.createElement('a');
 		tmp.innerHTML = toBeTOCced[i].innerHTML;
 		z.appendChild(tmp);
+		if (toBeTOCced[i].nodeName == 'H1')
+			tmp.className = 'b';
 		if (toBeTOCced[i].nodeName == 'H3')
-			tmp.className += ' indent';
+			tmp.className = 'indent';
 		if (toBeTOCced[i].nodeName == 'H4')
-			tmp.className += ' extraindent';
-		var headerId = toBeTOCced[i].id || 'link' + i;
+			tmp.className = 'extraindent';
+		var headerId = toBeTOCced[i].id || slugify(toBeTOCced[i].textContent);
 		tmp.href = '#' + headerId;
 		toBeTOCced[i].id = headerId;
 	}
@@ -29,8 +46,8 @@ function createTOC() {
 var TOCstate = 'none';
 
 function showhideTOC() {
-	TOCstate = (TOCstate == 'none') ? 'block' : 'none';
-	var newText = (TOCstate == 'none') ? '+ obsah' : '- obsah';
+	TOCstate = (TOCstate == 'none') ? 'block': 'none';
+	var newText = (TOCstate == 'none') ? '+ obsah': '- obsah';
 	document.getElementById('tocheader').innerHTML = newText;
 	document.getElementById('toc').lastChild.style.display = TOCstate;
 }
