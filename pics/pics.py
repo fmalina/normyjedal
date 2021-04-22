cb4bf46a94de64de929a218d2a038081efc2c40b
F. Malina <fmalina@pm.me>
2021-04-22 08:48:34 +0100

    Improved loader

--- pics/pics.py
+++ pics/pics.py
@@ -1,22 +1,35 @@
 import glob
 from lxml.html import fromstring
 from django.template.defaultfilters import slugify
-from instaloader import Instaloader
+import instaloader
+
+USER = 'blocl.uk'
 
 
-def get_hashtags(fn):
-    hashtags = []
+def headings(fn):
+    ls = []
     dom = fromstring(open(fn).read())
-    headings = dom.cssselect('h1,h2,h3')
-    for h in headings:
-        hashtags.append(slugify(h.text).replace('-',''))
-    return hashtags
+    elems = dom.cssselect('h1,h2,h3')
+    for h in elems:
+        ls.append(h.text)
+    return ls
 
 
 def load():
-    for fn in glob.glob('*.ht*'):
-        for hashtag in get_hashtags(fn):
-            Instaloader.download_hashtag(hashtag)
+    loader = instaloader.Instaloader()
+    loader.interactive_login(USER)
+    for fn in glob.glob('../*.htm'):
+        hs = headings(fn)
+        print(hs)
+        for h in hs:
+            hashtag = slugify(h).replace('-', '')
+            loader.download_hashtag(hashtag, max_count=10)
+
+
+def load_posts(loader, hashtag):
+    posts = instaloader.Hashtag.from_name(loader.context, hashtag).get_posts()
+    for post in posts:
+        loader.download_post(post, target=f'#{hashtag}')
 
 
 if __name__ == '__main__':
