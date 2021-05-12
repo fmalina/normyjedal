59d202844a315da673dc18d05e36d9344cb119d8
Francis Malina <fmalina@pm.me>
2021-05-12 02:03:26 +0100

    Dopln kredit k obrazkom

--- pictpl.py
+++ pictpl.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from django.template.defaultfilters import linebreaks
 from pics import get_hashtags
+import lzma
+import json
 
 hashtags = get_hashtags()
 docs_by_hashtag = dict([(y, (x, z)) for x, y, z in hashtags])
@@ -9,6 +11,27 @@ docs_by_hashtag = dict([(y, (x, z)) for x, y, z in hashtags])
 def render_text_files():
     for path in Path('pic').rglob('*.txt'):
         render(path)
+
+
+def clean_name(name):
+    no_digits = ''.join([x for x in name if not x.isdigit()])
+    return no_digits.replace('.', '').strip()
+
+
+def get_credit(path):
+    json_path = str(path).replace('.txt', '.json.xz')
+    try:
+        json_data = lzma.open(json_path).read()
+    except FileNotFoundError:
+        return ''
+    d = json.loads(json_data)
+    try:
+        username = d['node']['iphone_struct']['user']['username']
+    except KeyError:
+        return ''
+    if username:
+        return f' podľa @{username}'
+    return ''
 
 
 def render(path):
@@ -22,8 +45,8 @@ def render(path):
     hashtag = str(path).split('/')[-2]
     doc_name, recipe_name = docs_by_hashtag[hashtag]
     if recipe_name:
-        recipe_name = ''.join([x for x in recipe_name if not x.isdigit()]).replace('.', '').strip()
-
+        recipe_name = clean_name(recipe_name)
+    credit = get_credit(path)
     contents = f"""
 <!doctype html>
 <html lang="sk">
@@ -37,7 +60,7 @@ def render(path):
 <main>
     <p><img class="feature" src="{img_src}"></p>
     {desc}
-    <p><a href="/nom/{doc_name}#{hashtag}">{recipe_name}</a>
+    <p><a href="/nom/{doc_name}#{hashtag}">{recipe_name}</a>{credit}
 </main>
 </body>
 """
