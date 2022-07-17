60aa35da67db1fb0a7bea9ddaf8dfb8aecd4ce6f
F. Malina <fmalina@pm.me>
2022-07-17 02:20:24 +0200

    Update pictpl.py

--- _pics/pictpl.py
+++ _pics/pictpl.py
@@ -1,15 +1,39 @@
+"""
+Instagram picture webpage renderer.
+Renders webpages out of Instaloader text files to TARGET_PATH
+"""
+
 from pathlib import Path
 from django.template.defaultfilters import linebreaks
 from pics import get_hashtags
 import lzma
 import json
 
+TARGET_PATH = '../_assets/pic'
+TPL = """
+<!doctype html>
+<html lang="sk">
+<head>
+    <meta charset="utf8">
+    <link rel="stylesheet" href="/nom/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{rel_name} 📸 {title}</title>
+</head>
+<body>
+<main>
+    <p><img class="feature" src="{img_src}"></p>
+    <p><a href="/nom/{doc_name}#{hashtag}">{rel_name}</a>{credit}
+    {desc}
+</main>
+</body>
+"""
+
 hashtags = get_hashtags()
 docs_by_hashtag = dict([(y, (x, z)) for x, y, z in hashtags])
 
 
 def render_text_files():
-    for path in Path('../_assets/pic').rglob('*.txt'):
+    for path in Path(TARGET_PATH).rglob('*.txt'):
         render(path)
 
 
@@ -43,27 +67,11 @@ def render(path):
     desc = linebreaks(f)
     img_src = path_name.replace('.txt', '.jpg')
     hashtag = str(path).split('/')[-2]
-    doc_name, recipe_name = docs_by_hashtag[hashtag]
-    if recipe_name:
-        recipe_name = clean_name(recipe_name)
+    doc_name, rel_name = docs_by_hashtag[hashtag]
+    if rel_name:
+        rel_name = clean_name(rel_name)
     credit = get_credit(path)
-    contents = f"""
-<!doctype html>
-<html lang="sk">
-<head>
-    <meta charset="utf8">
-    <link rel="stylesheet" href="/nom/style.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{recipe_name} 📸 {title}</title>
-</head>
-<body>
-<main>
-    <p><img class="feature" src="{img_src}"></p>
-    <p><a href="/nom/{doc_name}#{hashtag}">{recipe_name}</a>{credit}
-    {desc}
-</main>
-</body>
-"""
+    contents = TPL.format(**locals())
     # print(str(path))
     fnw = str(path).replace('.txt', '.htm')
     fw = open(fnw, "w")
